35a2a039a9043ae885af5f2cbb34b72fe48ed027
andrewbartsch42 <andrewbartsch01@gmail.com>
2026-03-03 14:44:49 -0600

    added the global variables into the program

---
+++ faqs.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*
+ * Global Variables
+ * faqImage: main faq image element 
+ * faqImageORigSrc: Main FAQ image's default 'src'
+ * faqImageOrigAt1: Main FAQ image's alt text
+ * h2s: all h2 elements inside the FAQ section in the HTML
+ */
+const faqImage = document.getElementById("faq_image");
+const faqImageOrigSrc = faqImage.getAttribute(src);
+const faqImageOrigAtl = faqImage.getAttribute(alt);
+const h2s = document.getElementsByTagName("h2");
+
+
+
+

6464b8b67cfb3394d820a8277c3f88690e6570f6
andrewbartsch42 <andrewbartsch01@gmail.com>
2026-03-03 14:59:35 -0600

    added some logic for toggle function and also fixed global variable reference errors

--- faqs.js
+++ faqs.js
@@ -8,14 +8,26 @@
  * h2s: all h2 elements inside the FAQ section in the HTML
  */
 const faqImage = document.getElementById("faq_image");
-const faqImageOrigSrc = faqImage.getAttribute(src);
-const faqImageOrigAtl = faqImage.getAttribute(alt);
+const faqImageOrigSrc = faqImage.getAttribute("src");
+const faqImageOrigAtl = faqImage.getAttribute("alt");
 const h2s = document.querySelectorAll("#faqs h2");
 
 
 
 const toggleVisibility = evt => {
+    const userSelect = evt.currentTarget;
+    console.log(userSelect.id)
 
+    let allClosed = false; // don't know if i need this
+
+    for (h2 in h2s){
+        if (h2 === userSelect) { // this logic is wonk
+            // invert the state of the element
+            // update the image and alt text to the correct 
+        }
+
+
+    }
 }
 
 document.addEventListener("DOMContentLoaded", () =>{

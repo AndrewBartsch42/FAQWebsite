55656d310a3b7c5e366901dd55f4f833a935643e
andrewbartsch42 <andrewbartsch01@gmail.com>
2026-03-05 14:21:34 -0600

    added author header

--- faqs.js
+++ faqs.js
@@ -1,5 +1,7 @@
 "use strict";
-
+// Author: Andrew Bartsch
+// Github: https://github.com/AndrewBartsch42/FAQWebsite
+// Program Name: FAQWebsite
 /*
  * Global Variables
  * faqImage: main faq image element 
